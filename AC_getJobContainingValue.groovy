9d9604b0cd11cce78fd149c183e2a8bbbc68b289
Ioannis K. Moutsatsos <imoutsatsos@msn.com>
2019-06-18 10:41:59 -0400

    Support for case insensitive name search

--- AC_getJobContainingValue.groovy
+++ AC_getJobContainingValue.groovy
@@ -1,6 +1,6 @@
 /*** BEGIN META {
   "name" : "AC_getJobContainingValue",
-  "comment" : "This script displays jobs, containing the defined value",
+  "comment" : "This script returns job names, where JOB_NAME contains the defined text. Case Insensitive search",
   "parameters" : [ 'nameValue'],
   "core": "2.100",
   "authors" : [
@@ -15,7 +15,7 @@ hudsonInstance = hudson.model.Hudson.instance
 
 // Retrieve all Jobs which starts with -jobs-
 allItems = hudsonInstance.items
-chosenJobs = allItems.findAll{job -> job.name.contains(contains)}
+chosenJobs = allItems.findAll{job -> job.name.toUpperCase().contains(contains.toUpperCase())}
 
 // return names as a list of choices
 choices=[]
